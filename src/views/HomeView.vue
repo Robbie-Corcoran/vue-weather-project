e8e629c9ba6de3687570b5ed738920c3e7fe6f8e
Robbie Corcoran <codewithrobbie@gmail.com>
2024-02-03 17:17:01 +0100

    Searchbar displaying mapbox data correctly.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,18 @@
         placeholder="Search for a city"
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
       />
+      <ul
+        v-if="mapboxSearchReusults"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <li
+          v-for="searchResult in mapboxSearchReusults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
     </div>
   </main>
 </template>
