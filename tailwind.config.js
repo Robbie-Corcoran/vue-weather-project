a542f7f84b8cd3c9ebd87d5838f6806c16ec6784
Robbie Corcoran <codewithrobbie@gmail.com>
2024-02-03 14:29:46 +0100

    Further tailwind, font, fontawesome setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{vue, js, jsx, ts, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "weather-primary": "300668A",
+        "weather-secondary": "004E71",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    }
   },
   plugins: [],
 }
