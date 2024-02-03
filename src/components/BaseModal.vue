9ea1e49066fbf5ce9504e38d7e8a63da4ea4206d
Robbie Corcoran <codewithrobbie@gmail.com>
2024-02-03 15:23:51 +0100

    BaseModal enter and leave transitions setup

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,16 +1,43 @@
 <template>
-    <div class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 px-8">
-        <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
-            <slot />
-            <button class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
+  <Transition name="modal-outer">
+    <div
+      v-show="modalActive"
+      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 px-8"
+    >
+      <Transition name="modal-inner">
+        <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
+          <slot />
+          <button
+            class="text-white mt-8 bg-weather-primary py-2 px-6"
+            @click="$emit('close-modal')"
+          >
+            Close
+          </button>
         </div>
+      </Transition>
     </div>
+  </Transition>
 </template>
 
 <script setup>
+defineEmits(['close-modal'])
 
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false
+  }
+})
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
 
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
 </style>
