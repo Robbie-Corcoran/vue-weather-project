b8f189648d5c67482b8e243bf490a2503346b630
Robbie Corcoran <codewithrobbie@gmail.com>
2024-02-06 12:51:10 +0100

    Dynamic page titles

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Home'
+      }
     },
     {
       path: '/weather/:state/:city',
       name: 'cityView',
-      component: CityView
+      component: CityView,
+      meta: {
+        title: 'Weather'
+      }
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.state ? `${to.params.city}, ${to.params.state}` : to.meta.title
+  } | SkyGuard`;
+  next();
 });
 
 export default router;
