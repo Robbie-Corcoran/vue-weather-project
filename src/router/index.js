554cb6589ee6cf1d6baf0d288e184f21c47f793e
Robbie Corcoran <codewithrobbie@gmail.com>
2024-02-06 15:23:57 +0100

    Structure update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import CityView from '../views/CityView.vue';
+import CityView from '../views/CityView/CityView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
