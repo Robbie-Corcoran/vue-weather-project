96f1c5feedcd5c5c4f3647a6a692a30f9eaea4b5
Robbie Corcoran <codewithrobbie@gmail.com>
2024-02-06 21:51:43 +0100

    Created toggleunitbutton and working

--- src/components/SavedCityList.vue
+++ src/components/SavedCityList.vue
@@ -13,15 +13,10 @@
   </p>
 
   <button v-else class="flex flex-row-reverse">
-    <label class="relative inline-flex items-center me-5 cursor-pointer">
-      <input @click="toggleUnitPreference" type="checkbox" class="sr-only peer" checked />
-      <div
-        class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:bg-weather-secondary dark:peer-focus:bg-weather-secondary peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-weather-secondary"
-      ></div>
-      <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{{
-        unitPreference
-      }}</span>
-    </label>
+    <ToggleUnitButton
+      :unitPreference="unitPreference"
+      :toggleUnitPreference="toggleUnitPreference"
+    />
   </button>
 </template>
 
@@ -29,6 +24,7 @@
 import { ref } from 'vue';
 import axios from 'axios';
 import SavedCityCard from './SavedCityCard.vue';
+import ToggleUnitButton from './ToggleUnits/ToggleUnitButton.vue';
 import { useRouter } from 'vue-router';
 
 const savedCities = ref([]);
