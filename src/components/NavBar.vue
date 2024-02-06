89ebe20671b15b0ae15edd45b2542608a40eba0b
Robbie Corcoran <codewithrobbie@gmail.com>
2024-02-06 13:03:30 +0100

    Logo and favicon update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,8 +3,8 @@
     <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
       <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3 flex-1">
-          <img class="max-w-8" src="/sun.png" alt="" />
-          <p class="text-2xl">SkyGuard</p>
+          <img class="max-w-10" src="/sun.png" alt="" />
+          <p class="text-2xl pt-3">SkyGuard</p>
         </div>
       </RouterLink>
 
