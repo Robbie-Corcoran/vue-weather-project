d010dbb14f1f729ffbab29acb6209d1865487e2e
Robbie Corcoran <codewithrobbie@gmail.com>
2024-02-06 18:39:04 +0100

    Typo fix

--- src/components/SavedCityCard.vue
+++ src/components/SavedCityCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex py-6 px-3 bg-weather-secondary rounded-md shaodw-md cursor-pointer">
+  <div class="flex py-6 px-3 bg-weather-secondary rounded-md shadow-md cursor-pointer">
     <div class="flex flex-col flex-1">
       <h2 class="text-3xl">{{ city.city }}</h2>
       <h3>{{ city.state }}</h3>
